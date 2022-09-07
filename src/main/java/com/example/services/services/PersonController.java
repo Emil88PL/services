1f2286cdc516b5eb7348d9640233769860c3ec2d
Emil <emil88pl@gmail.com>
2022-09-07 13:50:43 +0100

    PersonalController CRUD working

--- src/main/java/com/example/services/services/PersonController.java
+++ src/main/java/com/example/services/services/PersonController.java
@@ -3,17 +3,61 @@ package com.example.services.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PersonController {
+public class PersonController<Person> {
 
-   // private List<Person> people = new ArrayList<>();
+    private List<Person> people = new ArrayList<>();
 
     @GetMapping("/test")
     public String test() {
         return "Hello, World!";
     }
+
+       // Create
+       @PostMapping("/create")
+       public boolean addPerson(@RequestBody Person person) {
+           return this.people.add(person);
+       }
+
+        // READ
+        @GetMapping("/getAll")
+        public List<Person> getAll() {
+            return this.people;
+        }
+
+        //Update
+        @PutMapping("/update")
+        public Person updatePerson(@PathParam("id") int id, @RequestBody Person person) {
+
+            // Remove existing Person with matching 'id'
+            this.people.remove(id);
+
+            // Add new Person in its place
+            this.people.add(id, person);
+            
+            // Return updated Person from List
+            return this.people.get(id);
+        }
+
+        //Delete
+        @DeleteMapping("/delete/{id}")
+        public Person removePerson(@PathVariable int id) {
+            // Remove Person and return it
+            return this.people.remove(id);
+        }
+
+
+
+
 
 }
